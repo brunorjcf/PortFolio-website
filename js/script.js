2e549d26edd78bc922836d4090cbc14dd882da73
Bruno Silva Dos Santos <Brunorjcf@gmail.com>
2022-08-24 00:19:14 -0300

    segundo commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-$('#menu').click(function(){
+$(document).ready(function(){
 
     $('#menu').click(function(){
-        $(this).toggleClass('fa-time');
-        $('header').toggleClass('toggle');
+
+        $('#menu').click(function(){
+            $(this).toggleClass('fa-times');
+            $('header').toggleClass('toggle');
+        });
+    
+        $(window).on('scroll load',function(){
+    
+            $('#menu').removeClass('fa-times');
+            $('header').removeClass('toggle');
+
+            if($(windows).scrollTop() > 0){
+                $('.top').show();
+            }else{
+                $('.top').hidden();
+            }
+        });
+
+    // smooth scrolling 
+    $('a[href*="#"]').on('click',function(e){
+
+        e.preventDefault();
+
+        $('html, body').animate({
+
+            scrollTop : $($(this).attr('href').offset().top,
+            )
+        },
+        500,
+        'linear'
+        );
     });
 
-    $(window).on('scroll load',function(){
-
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
     });
+  
 });
